2a35029e30d70f38e2fa713202ff683d1107df49
Blessed-Darko <2425400859@live.gctu.edu.gh>
2026-05-31 20:17:04 +0000

    Complete Assignment 2 with BDY initials

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: A program that prints stylized large initials (BDY) using only basic print statements.
+# Assignment Number: 2
+#
+# Name: Blessed Darko
+# STUDENT ID: 2425400859
+# Email: 2425400859@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Blessed Darko Yirenkyi, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # This function prints out small and stylized large initials with large periods.
+    print()
+    print("...BDY")
+    print()
+    print("...BBBBBBBBBB..........DDDDDDDDDD..........YY........YY.....")
+    print("...BB........BB........DD........DD........YY........YY.....")
+    print("...BB........BB........DD........DD.........YY......YY......")
+    print("...BB........BB........DD........DD.........YY......YY......")
+    print("...BBBBBBBBBB..........DD........DD..........YY....YY.......")
+    print("...BBBBBBBBBB..........DD........DD...........YY..YY........")
+    print("...BB........BB........DD........DD............YYYY.........")
+    print("...BB........BB........DD........DD.............YY..........")
+    print("...BB........BB...**...DD........DD...**........YY........**")
+    print("...BBBBBBBBBB.....**...DDDDDDDDDD.....**........YY........**")
+    print()
 
 
 main()
